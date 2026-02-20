4285586b204efcba3a3979278e98ea1105de599a
Veljko29 <veljko.ant2002@gmail.com>
2026-02-20 16:24:08 +0100

    Pocetak

---
+++ CurrencyExchange/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id, currency_from,currency_to,exchange_rate) 
+values(1,'EUR','EUR',1),
+(2,'EUR','USD',1.1),
+(3,'EUR','GBP',0.9),
+(4,'EUR','CHF',1.05),
+(5,'EUR','RSD',117);
