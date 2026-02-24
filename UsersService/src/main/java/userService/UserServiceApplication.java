4400e48bd58993ae55a409bf2ea0218afa01f1ea
Veljko29 <veljko.ant2002@gmail.com>
2026-02-24 20:09:07 +0100

    Dodati BankAccount, CryptoWallet, unapredjen UserService, neuspela 3 pokusaja komita pre ovog

---
+++ UsersService/src/main/java/userService/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"util.exceptions", "usersService"})
+@EnableFeignClients(basePackages = {"api.proxies"})
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
